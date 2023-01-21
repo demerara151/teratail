ea6ee5d68587f9a488b20055c3041f7e147983b0
demerara151 <demeraraonfire@gmail.com>
2023-01-22 00:28:17 +0900

    add crawling functionality to navitime.py

--- src/navitime.py
+++ src/navitime.py
@@ -2,24 +2,48 @@
 Pythonでselenium 次のページをクリック
 https://teratail.com/questions/pgd3cgpf1toqit
 """
-# type: ignore
 import asyncio
 
 from playwright.async_api import async_playwright
 
-url: str = "https://www.navitime.co.jp/category/0805/11324/"
+url: str = "https://www.navitime.co.jp/category/0805/11324?page={}"
 
 
 async def main(url: str) -> None:
+    page_number: int = 1
+    query: str = "poi?spot"
+    result: list[str] = []
     async with async_playwright() as p:
         browser = await p.chromium.launch()
         page = await browser.new_page()
-        await page.goto(url)
-        # find out total pages
-        # add query to url
-        # select url element
-        # extract and gather url
-        await browser.close()
+
+        while True:
+            res = await page.goto(url.format(page_number))
+
+            if res is None or res.status == 404:
+                print("End of the pages.")
+                await browser.close()
+                break
+            elif res is not None and res.status == 200:
+                result.extend(
+                    [
+                        link
+                        for link in [
+                            await anchor.get_attribute("href")
+                            for anchor in await page.get_by_role("link").all()
+                        ]
+                        if link is not None
+                        if query in link
+                    ]
+                )
+                page_number += 1
+                continue
+            else:
+                print("[Error]: Something's happened.")
+                await browser.close()
+                break
+
+        print(result)
 
 
 if __name__ == "__main__":
