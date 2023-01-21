f22a2647de3217252f59f3ce973002efcb665846
demerara151 <demeraraonfire@gmail.com>
2023-01-21 18:35:41 +0900

    add comment

--- teratail/kamui.py
+++ teratail/kamui.py
@@ -3,14 +3,17 @@ import asyncio
 
 import httpx
 
+# 開発者ツールのネットワークタブから、API のアドレスを取得
 url: str = (
     "https://app.kamuitracker.com/kamunavi/v1/channels/"
     "search?date=20230121&page={}"
 )
-pages = range(100, 201)
+pages = range(1, 1593)
 
 
 async def fetch(page: int) -> None:
+    "該当の URL にリクエストを送信し、返ってきた値を TSV に書き込む"
+
     async with httpx.AsyncClient(
         headers={
             "Accept": "application/json",
@@ -24,14 +27,19 @@ async def fetch(page: int) -> None:
         res = await client.get(url.format(page))
         res.raise_for_status()
         json = res.json()
-        # await asyncio.sleep(0.5)
+        await asyncio.sleep(1)
 
+        # チャンネル名にコンマが含まれていることがあるため、CSV ではなく TSV を使用
         with open(f"database/{page:04}_ranking.tsv", "w") as f:
             for item in json["item"]:
                 f.write(f'{item["rank"]}\t{item["title"]}\n')
 
 
 async def main() -> None:
+    """
+    並列処理で加速
+    複数のファイルに分かれることになるが、後から結合すればいい
+    """
     async with asyncio.TaskGroup() as tg:
         for page in pages:
             tg.create_task(fetch(page))
