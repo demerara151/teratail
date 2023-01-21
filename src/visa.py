3c6f78478cadc2b8eb61f66f9b0028d8a3cdea9b
demerara151 <demeraraonfire@gmail.com>
2023-01-21 22:31:46 +0900

    add playwright example

---
+++ src/visa.py
@@ -0,0 +1,37 @@
+# type: ignore
+import asyncio
+
+from playwright.async_api import async_playwright
+
+url = (
+    "https://usa.visa.com/support/consumer/travel-support/"
+    "exchange-rate-calculator.html"
+)
+amount = "58742"
+
+
+async def main():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+        page = await browser.new_page()
+        await page.goto(url)
+        await page.get_by_placeholder("Enter amount").fill(amount)
+
+        await page.get_by_role(
+            "button", name="Open from currency list drop down menu."
+        ).click()
+        await page.locator("#listbox-item-2").click()
+        await page.get_by_role(
+            "button", name="Open to currency list drop down menu."
+        ).click()
+        await page.locator("#listbox-item-0").click()
+
+        await page.get_by_role("button", name=" Calculate Conversion ").click()
+
+        print(await page.locator("h2.vs-h2").nth(1).text_content())
+        print(await page.locator("dm-calculator div").nth(45).text_content())
+
+        await browser.close()
+
+
+asyncio.run(main())
