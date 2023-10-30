bb0c7acd5ec35dc1b85b9617cdf69e5a23764daa
demerara151 <demeraraonfire@gmail.com>
2023-10-30 20:57:04 +0900

    fix:🐛 fix csv header is written each data
    ついでにコードのリファクタリングを実行
    - クラスを定義
    - 一部の変数名をよりわかりやすく変更
    - URL の渡し方を変更
    - 内包表記を利用

--- src/kamui.py
+++ src/kamui.py
@@ -3,53 +3,58 @@ seleniumでスクレイピング。ある部分が取れない
 https://teratail.com/questions/kwcuzshawbpplv
 """
 import asyncio
-import csv
+from dataclasses import dataclass
 
 import httpx
 
-# 開発者ツールのネットワークタブから、API のアドレスを取得
-url: str = (
-    "https://app.kamuitracker.com/kamunavi/v1/channels/"
-    "search?date=20230121&page={}"
-)
-pages = range(1, 1593)
 
+@dataclass(slots=True, frozen=True)
+class Kamui:
+    pages = range(1, 1593)
 
-async def fetch(page: int) -> list[tuple[int, str]]:
-    async with httpx.AsyncClient(
-        headers={
-            "Accept": "application/json",
-            "User-Agent": (
-                "Mozilla/5.0 (Windows NT 10.0; rv:109.0)"
-                " Gecko/20100101 Firefox/109.0"
-            ),
-        },
-        timeout=10.0,
-    ) as client:
-        res = await client.get(url.format(page))
-        res.raise_for_status()
-        json = res.json()
-        await asyncio.sleep(1)
+    async def fetch(self, page_number: int) -> list[tuple[int, str]]:
+        # cSpell: disable
+        async with httpx.AsyncClient(
+            headers={
+                "Accept": "application/json",
+                "User-Agent": (
+                    "Mozilla/5.0 (Windows NT 10.0; rv:109.0)"
+                    " Gecko/20100101 Firefox/119.0"
+                ),
+            },
+            timeout=10.0,
+            base_url="https://app.kamuitracker.com",
+        ) as client:
+            response = await client.get(
+                url="kamunavi/v1/channels/search",
+                params={"date": "20231030", "page": page_number},
+            )
+            response.raise_for_status()
+            json = response.json()
+            await asyncio.sleep(1)
 
-        return [(item["rank"], item["title"]) for item in json["item"]]
+            return [(item["rank"], item["title"]) for item in json["item"]]
 
-
-async def main() -> list[list[tuple[int, str]]]:
-    results: list[asyncio.Task[list[tuple[int, str]]]] = []
-    async with asyncio.TaskGroup() as tg:
-        for page in pages:
-            task = tg.create_task(fetch(page))
-            results.append(task)
-    return [task.result() for task in results]
+    async def main(self) -> list[list[tuple[int, str]]]:
+        async with asyncio.TaskGroup() as tg:
+            tasks = [
+                tg.create_task(self.fetch(page_number))
+                for page_number in self.pages
+            ]
+        return [task.result() for task in tasks]
 
 
 if __name__ == "__main__":
-    results = asyncio.run(main())
+    import csv
+
+    kamui = Kamui()
+    results = asyncio.run(kamui.main())
     results.sort()
-    for res in results:
-        with open("ranking.csv", "a", newline="") as file:
-            # use tab string instead of comma
-            # because some channel name has comma
-            csv_out = csv.writer(file, delimiter="\t")
-            csv_out.writerow(["rank", "channel"])
-            csv_out.writerows(res)
+
+    with open("ranking.csv", "a", newline="") as file:
+        # use tab string instead of comma
+        # because some channel name has comma
+        csv_writer = csv.writer(file, delimiter="\t")
+        csv_writer.writerow(["rank", "channel"])
+
+        [csv_writer.writerows(result) for result in results]
