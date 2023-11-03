ab29d613909f6c7804fa15528dc51c9718d09ea1
demerara151 <demeraraonfire@gmail.com>
2023-11-03 19:39:44 +0900

    refactor:💄 set driver as class property

--- src/lost_elements.py
+++ src/lost_elements.py
@@ -7,21 +7,21 @@ import time
 from dataclasses import dataclass
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 @dataclass(slots=True, frozen=True)
 class Crawler:
     url: str
-    driver: webdriver.Chrome
 
-    def extract_info(self) -> str:
-        "Extract data on the page."
-        return self.driver.title
+    @property
+    def _driver(self) -> webdriver.Chrome:
+        "chromedriver"
+        driver = webdriver.Chrome()
+        driver.implicitly_wait(30)
+        return driver
 
     def main(self) -> list[str]:
         """
@@ -31,51 +31,47 @@ class Crawler:
 
         Loop this until the links of first page are exhausted.
         """
-        self.driver.get(self.url)
-        wait = WebDriverWait(driver, 10)
+        with self._driver as driver:
+            driver.get(self.url)
+            wait = WebDriverWait(driver, 10)
 
-        # 最初のページのウィンドウ情報を記録しておく
-        original_window = self.driver.current_window_handle
+            # 最初のページのウィンドウ情報を記録しておく
+            original_window = driver.current_window_handle
 
-        results: list[str] = []
+            results: list[str] = []
 
-        elements = self.driver.find_elements(
-            By.CSS_SELECTOR, "#techblog ul li a"
-        )
+            elements = driver.find_elements(
+                By.CSS_SELECTOR, "#techblog ul li a"
+            )
 
-        for element in elements:
-            element.click()
+            for element in elements:
+                driver.execute_script("arguments[0].click();", element)
 
-            # 新しいウインドウかタブが開くまで待機
-            wait.until(EC.number_of_windows_to_be(2))
+                # 新しいウインドウかタブが開くまで待機
+                wait.until(EC.number_of_windows_to_be(2))
 
-            # 新しいウインドウハンドルを見つけるまでループする
-            for window_handle in self.driver.window_handles:
-                if window_handle != original_window:
-                    self.driver.switch_to.window(window_handle)
-                    result = self.extract_info()
-                    results.append(result)
-                    break
+                # 新しいウインドウハンドルを見つけるまでループする
+                for window_handle in driver.window_handles:
+                    if window_handle != original_window:
+                        driver.switch_to.window(window_handle)
+                        result = driver.title
+                        results.append(result)
+                        break
 
-            # 抽出が終わったタブを閉じる
-            self.driver.close()
+                # 抽出が終わったタブを閉じる
+                driver.close()
 
-            # サイトへの負荷軽減のため数秒待機
-            time.sleep(2)
+                # サイトへの負荷軽減のため数秒待機
+                time.sleep(2)
 
-            # 一番最初のタブに戻る
-            self.driver.switch_to.window(original_window)
+                # 一番最初のタブに戻る
+                driver.switch_to.window(original_window)
 
-        return results
+            return results
 
 
 if __name__ == "__main__":
     URL: str = "https://gihyo.jp/"
-    SERVICE = Service(ChromeDriverManager().install())
-    OPTIONS = webdriver.ChromeOptions()
-
-    with webdriver.Chrome(service=SERVICE, options=OPTIONS) as driver:
-        driver.implicitly_wait(30)
-        crawler = Crawler(URL, driver)
-        result: list[str] = crawler.main()
-        print(result)
+    crawler = Crawler(URL)
+    result: list[str] = crawler.main()
+    print(result)
